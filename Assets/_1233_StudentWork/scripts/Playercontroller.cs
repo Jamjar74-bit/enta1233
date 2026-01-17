78ddf4943fefe506363320753c4ee12156b308eb
Jamjar74-bit <rocketroll74@gmail.com>
2026-01-16 17:48:02 -0800

    Assigned Playerinput and Character Controller
    Assigned Playerinput and Character Controller

--- Assets/_1233_StudentWork/scripts/Playercontroller.cs
+++ Assets/_1233_StudentWork/scripts/Playercontroller.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+
 public class Playercontroller : MonoBehaviour
 {
 
     private Vector2 _input;
+    private CharacterController _CharacterController;
+    private Vector3 _direction;
+
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        _CharacterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _CharacterController.Move(_direction * speed * Time.deltaTime);
+    }
     public void Move(InputAction.CallbackContext context)
     {
 
+
+
         _input = context.ReadValue<Vector2>();
-        Debug.Log(_input);
-        
-
-
-
-
+        _direction = new Vector3(_input.x, y: 0.0f, z: _input.y);
 
 
 
@@ -27,5 +41,5 @@ public class Playercontroller : MonoBehaviour
 
 
 
-}
+     }
 }
